83307fbca37a6d3365af10a460e928492fd42da3
Ray1457 <134600651+Ray1457@users.noreply.github.com>
2025-04-29 11:35:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, session, jsonify, request    
-import json
+import json, os
 import stripe
 
 
@@ -261,5 +261,5 @@ def checkout_success():
 def checkout_cancel():
     return "Payment cancelled."
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
