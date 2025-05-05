9f4325b75e65d35f4be0c9bdc142449318c84da7
Ray1457 <134600651+Ray1457@users.noreply.github.com>
2025-05-05 21:25:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -273,5 +273,5 @@ def checkout_cancel():
     return "Payment cancelled."
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    # app.run(debug=True, port=5000)
